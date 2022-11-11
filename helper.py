1f05289e4fe6ad62eabe009c6cdde055d28c7437
jyots000 <86598536+jyots000@users.noreply.github.com>
2022-11-11 11:28:18 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -4,7 +4,7 @@ import re
 import json
 
 
-def create_incident(category,impact,desc,urgency,vmanage_alert_id):
+def create_data(category,impact,desc,urgency,vmanage_alert_id):
     # create incident in ServiceNow
     headers = {"Content-Type":"application/json", "Accept":"application/json"}
     auth = (os.environ['SERVICENOW_USERNAME'], os.environ['SERVICENOW_PASSWORD'])
@@ -21,14 +21,8 @@ def create_incident(category,impact,desc,urgency,vmanage_alert_id):
         "description": "The full log for this alert is:  \n" + desc
     }
     try:
-        create_ticket = requests.post(os.environ['SERVICENOW_INSTANCE'] + "/api/now/table/incident", auth=auth, headers=headers, json=ticket)
+        create_ticket = requests.post(os.environ['SERVICENOW_INSTANCE'] + "/api/now/table/u_evtmgmt_staging_table", auth=auth, headers=headers, json=ticket)
     except Exception as e:
         return "Error on Creating ServiceNow Ticket! Check your ServiceNow settings or connectivity!"
     servicenow_raw_json = create_ticket.json()
 
-    if os.environ['WEBEX_NOTIFICATION'] == '1':
-        try:
-            send_notification("Incident Created", servicenow_raw_json["result"]["sys_id"], desc)
-        except Exception as e:
-            return "Error on Sending Webex Message!If it is not intended to use turn it from enviromental variables!" + print(e)
-    return create_ticket.status_code
